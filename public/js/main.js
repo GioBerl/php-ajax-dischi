8f9b2a2bc319b9dbdbcba6723194d6a2c0faeaa2
Gio <giovanni.berl1@gmail.com>
2020-06-17 21:19:57 +0200

    in versione ajax aggiunto filtro per genere anziche per artista perche sono furbo

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     var templateFunc = Handlebars.compile($("#card-template").html());
+    var templateSelect = Handlebars.compile($("#select-template").html());
+    var arrayGenre = [];
 
     localUrl =
         "http://localhost:8888/boolean-php/php-ajax-dischi/versione-ajax/dischi-ajax.php";
@@ -9,15 +11,49 @@ $(document).ready(function () {
         url: localUrl,
         success: function (response) {
             stampaCard(response);
+            generaOptions(response);
         },
         error: function () {
             console.log("errore");
         },
     });
 
+    //AL CAMBIO DELLA SELECT
+    $("#mySelect").on("change", function () {
+        var optionSelected = $(this).find("option:selected").val();
+        console.log(optionSelected);
+        if (optionSelected) {
+            $(".card").hide();
+            // console.log(optionSelected);
+            // console.log($(`.card[data-genre=${optionSelected}]`));
+            $(`.card[data-genre=${optionSelected}]`).show();
+        } else {
+            $(".card").show();
+        }
+    });
+
+    function generaOptions(arrayDati) {
+        for (var i = 0; i < arrayDati.length; i++) {
+            var singleObj = arrayDati[i];
+
+            if (!arrayGenre.includes(singleObj.genre)) {
+                arrayGenre.push(singleObj.genre);
+            }
+        }
+
+        for (var j = 0; j < arrayGenre.length; j++) {
+            var context = {
+                genre: arrayGenre[j],
+            };
+            var html = templateSelect(context);
+            $("#mySelect").append(html);
+        }
+    }
+
     function stampaCard(arrayDati) {
         for (var i = 0; i < arrayDati.length; i++) {
             var singleObj = arrayDati[i];
+
             var context = {
                 title: singleObj.title,
                 author: singleObj.author,
@@ -25,6 +61,7 @@ $(document).ready(function () {
                 poster: singleObj.poster,
                 genre: singleObj.genre,
             };
+
             var html = templateFunc(context);
             $("main").append(html);
         }
